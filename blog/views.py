994d0a6f46b6a5d493dc365041131bee28e10bb0
mark odhiambo okoth <markokoth96@gmail.com>
2019-12-18 19:25:41 +0300

    models addd

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . models import Post
 
 
 def home(request):
-    return render(request, 'pages/index.html')
-    
+
+    return render(request, 'pages/index.html',)
+
+
 @login_required
 def blog(request):
-    return render(request, 'blog/blog.html' )
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/blog.html', context)
 
 def about(request):
     return render(request, 'about/about.html')    
