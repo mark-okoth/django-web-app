0dbe38b230705bf3fadeeb6711f5434cbee81029
mark odhiambo okoth <markokoth96@gmail.com>
2019-12-19 09:18:07 +0300

    blog design change dates and all other parts parts of the blog page

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 from . import urls
 from. import views
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f' Account created for{username}')
             return redirect('login')
     else:
-        form = UserCreationForm()        
-    
-    return render(request, 'accounts/register.html', {'form': form})
+        form = UserRegisterForm()
 
+    return render(request, 'accounts/register.html', {'form': form})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('', include('accounts.urls')),
     path('login/', auth_views.LoginView.as_view(), name="login" ),
-    path('accounts/login/', auth_views.LoginView.as_view(), name="login" ),
+    path('login/', auth_views.LoginView.as_view(), name="login" ),
     path('logout/',auth_views.LogoutView.as_view(), name="logout" ),
 ]
